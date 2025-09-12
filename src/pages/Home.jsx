256dfd961eb25945340c17b14de9c6321b98865e
Harshit Jaiswal <hjaiswal285@gmail.com>
2025-09-12 17:07:32 +0530

    Improve Home page styling with Tailwind

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ProductList from './ProductList'
+export default function Home(){
+  return (
+    <div>
+      <h1 className="page-title">Featured Products</h1>
+      <ProductList />
+    </div>
+  )
+}
